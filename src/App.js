66e8c1f251ee06c4d53ca62eed3336c8b532362f
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-07-28 21:04:33 +0600

    All dependencies  are working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Web3ReactProvider, useWeb3React } from '@web3-react/core';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { ethers } from 'ethers';
+
+const injectedConnector = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42], // Supported Ethereum network chain IDs
+});
+
+function getLibrary(provider) {
+  return new ethers.providers.Web3Provider(provider);
+}
+
+function Wallet() {
+  const { chainId, account, activate, active } = useWeb3React();
+
+  const connectWallet = () => {
+    activate(injectedConnector);
+  };
+
+  return (
+    <div>
+      <h2>Web3ReactProvider Test</h2>
+      <div>Chain ID: {chainId}</div>
+      {active ? (
+        <div>
+          <p>Connected Account: {account}</p>
+        </div>
+      ) : (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Wallet />
+    </Web3ReactProvider>
   );
 }
 
