29385cad222e46e16ba2de80c6a1723a805056b3
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-08-21 20:08:39 +0600

    got it running with wagami wallet connect

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { Web3ReactProvider, useWeb3React } from '@web3-react/core';
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { ethers } from 'ethers';
+import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
+import { connect, getAccount  } from '@wagmi/core'
+
 
 const injectedConnector = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42], // Supported Ethereum network chain IDs
 });
+
+const wcConnector = new WalletConnectConnector({
+  options: { projectId: "47ace990d320fd976c47f6169a894dbf", showQrModal: true },
+})
 
 function getLibrary(provider) {
   return new ethers.providers.Web3Provider(provider);
@@ -14,8 +21,19 @@ function getLibrary(provider) {
 function Wallet() {
   const { chainId, account, activate, active } = useWeb3React();
 
-  const connectWallet = () => {
-    activate(injectedConnector);
+  const connectWallet = async () => {
+    // activate(injectedConnector);
+    console.log(wcConnector.ready)
+    try {
+      await activate(wcConnector)
+    console.log(wcConnector.connect(chainId))
+    // const account = getAccount()
+    // console.log(account)
+      
+      
+    } catch (error) {
+      console.error(error)
+    }
   };
 
   return (
