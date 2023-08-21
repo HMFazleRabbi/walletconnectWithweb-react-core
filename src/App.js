ae6ca569705d022670062bd179ad289acebc4d34
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-08-22 04:25:06 +0600

    Complete support

--- src/App.js
+++ src/App.js
@@ -1,52 +1,146 @@
-import React from 'react';
-import { Web3ReactProvider, useWeb3React } from '@web3-react/core';
-import { InjectedConnector } from '@web3-react/injected-connector';
-import { ethers } from 'ethers';
-import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
-import { connect, getAccount  } from '@wagmi/core'
+import React, { useState } from "react";
+import { Web3ReactProvider, useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
+import { WalletConnectConnector } from "@wagmi/core/connectors/walletConnect";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import "./App.css";
 
+const walletconnectConnector = new WalletConnectConnector({
+  options: { projectId: "47ace990d320fd976c47f6169a894dbf", showQrModal: true },
+});
 
 const injectedConnector = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42], // Supported Ethereum network chain IDs
 });
-
-const wcConnector = new WalletConnectConnector({
-  options: { projectId: "47ace990d320fd976c47f6169a894dbf", showQrModal: true },
-})
 
 function getLibrary(provider) {
   return new ethers.providers.Web3Provider(provider);
 }
 
 function Wallet() {
-  const { chainId, account, activate, active } = useWeb3React();
+  const {
+    account,
+    activate,
+    active,
+    chainId,
+    connector,
+    deactivate,
+    error,
+    provider,
+    setError,
+  } = useWeb3React();
 
-  const connectWallet = async () => {
-    // activate(injectedConnector);
-    console.log(wcConnector.ready)
+  const connectWalletConnectWallet = async () => {
     try {
-      await activate(wcConnector)
-    console.log(wcConnector.connect(chainId))
-    // const account = getAccount()
-    // console.log(account)
-      
-      
+      console.log("Wallet connect triggered");
+      await walletconnectConnector.connect(chainId);
+      let accountWC = await walletconnectConnector.getAccount();
+      let chainIdWC = await walletconnectConnector.getChainId();
+      setconnectorName("Wallet Connect");
+      SetwcAccount(accountWC);
+      setWcChainId(chainIdWC);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
+  const disconnectWalletConnectWallet = async () => {
+    try {
+      console.log("Wallet disconnectWalletConnectWallet triggered");
+      await walletconnectConnector.disconnect();
+      setconnectorName(null);
+      SetwcAccount(null);
+      setWcChainId(null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = () => {
+    console.log("Meta connect", active);
+    activate(injectedConnector);
+    setconnectorName("Metamask");
+  };
+
+  const disconnectWallet = async () => {
+    deactivate();
+    localstorage.removeitem("account");
+    setconnectorName(null);
+  };
+
+  const [connectorName, setconnectorName] = useState(null);
+  const [wcAccount, SetwcAccount] = useState(null);
+  const [wcChainId, setWcChainId] = useState(null);
+
   return (
-    <div>
-      <h2>Web3ReactProvider Test</h2>
-      <div>Chain ID: {chainId}</div>
-      {active ? (
-        <div>
-          <p>Connected Account: {account}</p>
+    <div className="wallet-container">
+      <div className="card">
+        <h2 className="wallet-title">
+          {connectorName ? `Connection Status` : "Connect Using"}
+        </h2>
+        <div className="card-content">
+          {connectorName === "Metamask" ? (
+            <div className="wallet-connected">
+              <div className="chain-id">
+                <span className="bold-text">Chain ID:</span> {chainId}
+              </div>
+              <div className="connected-account">
+                <span className="bold-text">Connected Account:</span> {account}
+              </div>
+              <div className="disconnect-button-container">
+                <button
+                  className="disconnect-button"
+                  onClick={disconnectWallet}
+                >
+                  Disconnect
+                </button>
+              </div>
+              <div className="footer">
+                {connectorName && <p>Connected using {connectorName}</p>}
+              </div>
+            </div>
+          ) : connectorName === "Wallet Connect" ? (
+            <div className="wallet-connected">
+              <div className="chain-id">
+                <span className="bold-text">Chain ID:</span> {wcChainId}
+              </div>
+              <div className="connected-account">
+                <span className="bold-text">Connected Account:</span>
+                <div> {wcAccount} </div>
+              </div>
+              <div className="disconnect-button-container">
+                <button
+                  className="disconnect-button"
+                  onClick={disconnectWalletConnectWallet}
+                >
+                  Disconnect
+                </button>
+              </div>
+              <div className="footer">
+                {connectorName && <p>Connected using {connectorName}</p>}
+              </div>
+            </div>
+          ) : (
+            <div className="wallet-not-connected">
+              <button
+                className={`connect-button ${
+                  !window.ethereum ? "disabled" : ""
+                }`}
+                onClick={connectWallet}
+                disabled={!window.ethereum}
+              >
+                Metamask
+              </button>
+              <button
+                className="connect-wallet-button"
+                onClick={connectWalletConnectWallet}
+              >
+                Wallet Connect
+              </button>
+            </div>
+          )}
         </div>
-      ) : (
-        <button onClick={connectWallet}>Connect Wallet</button>
-      )}
+      </div>
     </div>
   );
 }
